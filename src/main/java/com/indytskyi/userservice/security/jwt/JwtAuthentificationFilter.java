16fd69afdd02d69ac14b99ceddf42ab1f63423e0
Indytskyi Artem <ind.artem2003@gmail.com>
2023-04-06 16:45:45 +0300

    create class JwtService

--- src/main/java/com/indytskyi/userservice/security/jwt/JwtAuthentificationFilter.java
+++ src/main/java/com/indytskyi/userservice/security/jwt/JwtAuthentificationFilter.java
@@ -1,5 +1,6 @@
 package com.indytskyi.userservice.security.jwt;
 
+import com.indytskyi.userservice.service.JwtService;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
@@ -14,6 +15,12 @@ import org.springframework.web.filter.OncePerRequestFilter;
 @Component
 @RequiredArgsConstructor
 public class JwtAuthentificationFilter extends OncePerRequestFilter {
+
+    private static final String BEARER_START = "Bearer ";
+    private static final int TOKEN_START_INDEX = 7;
+
+    private final JwtService jwtService;
+
     @Override
     protected void doFilterInternal(
             @NotNull HttpServletRequest request,
@@ -22,11 +29,13 @@ public class JwtAuthentificationFilter extends OncePerRequestFilter {
         final String authHeader = request.getHeader("Authorization");
         final String jwt;
         final String userEmail;
-        if (authHeader == null || !authHeader.startsWith("Bearer ")) {
+        if (authHeader == null || !authHeader.startsWith(BEARER_START)) {
             filterChain.doFilter(request, response);
             return;
         }
 
+        jwt = authHeader.substring(TOKEN_START_INDEX);
+        userEmail = jwtService.extractUsername(jwt);
 
     }
 }
