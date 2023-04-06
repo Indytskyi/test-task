56c930a8042789405841b3487b040e221abe9c48
Indytskyi Artem <ind.artem2003@gmail.com>
2023-04-06 16:20:44 +0300

    create repository for User

---
+++ src/main/java/com/indytskyi/userservice/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package com.indytskyi.userservice.repository;
+
+import com.indytskyi.userservice.models.User;
+import java.util.Optional;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByEmail(String email);
+}
