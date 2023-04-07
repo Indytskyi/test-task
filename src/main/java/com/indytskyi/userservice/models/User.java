8d8ca1522be64ab99492e7bf40070101d759b415
Indytskyi Artem <ind.artem2003@gmail.com>
2023-04-07 12:06:38 +0300

    make field name unique

--- src/main/java/com/indytskyi/userservice/models/User.java
+++ src/main/java/com/indytskyi/userservice/models/User.java
@@ -40,7 +40,7 @@ public class User implements UserDetails {
     @Column(nullable = false)
     private String password;
 
-    @Column(nullable = false)
+    @Column(nullable = false, unique = true)
     private String name;
 
     @Column(nullable = false)
