1623c825a23828ee40dd7e98f56964a143dda4ce
Indytskyi Artem <ind.artem2003@gmail.com>
2023-04-07 15:03:48 +0300

    create annotation to valid enum Color

--- src/main/java/com/indytskyi/userservice/dtos/ArticleRequestDto.java
+++ src/main/java/com/indytskyi/userservice/dtos/ArticleRequestDto.java
@@ -1,5 +1,7 @@
 package com.indytskyi.userservice.dtos;
 
+import com.indytskyi.userservice.annotation.ValidEnum;
+import com.indytskyi.userservice.models.enums.Color;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.Pattern;
 import lombok.AllArgsConstructor;
@@ -13,11 +15,9 @@ import org.jetbrains.annotations.NotNull;
 @AllArgsConstructor
 @NoArgsConstructor
 public class ArticleRequestDto {
-
     @NotEmpty
     private String text;
-
     @NotNull
-    @Pattern(regexp = " GREEN|RED|BLACK|BLUE", message = "Incorrect color")
+    @ValidEnum(enumClass = Color.class)
     private String color;
 }
