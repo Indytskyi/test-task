8de772de8810d39ae8af55af30a71be02cfb2604
Indytskyi Artem <ind.artem2003@gmail.com>
2023-04-08 14:53:40 +0300

    add validation in RegisterRequestDto in field name, age

--- src/main/java/com/indytskyi/userservice/dtos/request/RegisterRequestDto.java
+++ src/main/java/com/indytskyi/userservice/dtos/request/RegisterRequestDto.java
@@ -3,6 +3,7 @@ package com.indytskyi.userservice.dtos.request;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
@@ -16,13 +17,16 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public class RegisterRequestDto {
   @Email
+  @NotEmpty
   private String email;
   @Pattern(regexp = "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&+=])(?=\\S+$).{8,}$",
           message = "Incorrect format of password")
   @NotEmpty
   private String password;
   @Size(min = 3, message = "Input correct firstName")
+  @NotEmpty
   private String name;
   @Min(value = 5, message = "The minimum age for creating an account must be over 5")
+  @NotNull
   private Integer age;
 }
