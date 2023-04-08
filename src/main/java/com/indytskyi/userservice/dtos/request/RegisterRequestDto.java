e1146dcf7f2b86564da993470286d3004f8390ad
Indytskyi Artem <ind.artem2003@gmail.com>
2023-04-08 14:49:02 +0300

    add validation to password NotEmpty

--- src/main/java/com/indytskyi/userservice/dtos/request/RegisterRequestDto.java
+++ src/main/java/com/indytskyi/userservice/dtos/request/RegisterRequestDto.java
@@ -2,6 +2,7 @@ package com.indytskyi.userservice.dtos.request;
 
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
@@ -18,6 +19,7 @@ public class RegisterRequestDto {
   private String email;
   @Pattern(regexp = "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&+=])(?=\\S+$).{8,}$",
           message = "Incorrect format of password")
+  @NotEmpty
   private String password;
   @Size(min = 3, message = "Input correct firstName")
   private String name;
