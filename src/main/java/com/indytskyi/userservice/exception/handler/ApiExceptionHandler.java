badfb1714b10d14e8c9d98e87bc6301c18bc6846
Indytskyi Artem <ind.artem2003@gmail.com>
2023-04-08 15:02:12 +0300

    handle exception if user login with bad credentials

--- src/main/java/com/indytskyi/userservice/exception/handler/ApiExceptionHandler.java
+++ src/main/java/com/indytskyi/userservice/exception/handler/ApiExceptionHandler.java
@@ -14,6 +14,7 @@ import java.util.stream.Collectors;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -21,11 +22,7 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 @RestControllerAdvice
 @Slf4j
 public class ApiExceptionHandler {
-    /**
-     * If we get not valid arguments.
-     * @param e = object of exception
-     * 
-     */
+
     @ExceptionHandler
     private ResponseEntity<List<ErrorResponse>> handleException(MethodArgumentNotValidException e) {
         log.error("Not valid arguments, throw = {}", e.getClass());
@@ -42,17 +39,18 @@ public class ApiExceptionHandler {
     })
     public ResponseEntity<ApiExceptionObject> handleUserNorFoundException(RuntimeException e) {
         HttpStatus status = HttpStatus.NOT_FOUND;
-        return new ResponseEntity<>(getApiExceptionObject(e.getMessage(), status), status);
+        return new ResponseEntity<>(createApiExceptionResponse(e.getMessage(), status), status);
     }
 
     @ExceptionHandler(value = {
-            LimitedPermissionException.class
+            LimitedPermissionException.class,
+            BadCredentialsException.class
     })
     public ResponseEntity<ApiExceptionObject> handleTokenAccessException(
             RuntimeException e
     ) {
         HttpStatus status = HttpStatus.FORBIDDEN;
-        return new ResponseEntity<>(getApiExceptionObject(e.getMessage(), status), status);
+        return new ResponseEntity<>(createApiExceptionResponse(e.getMessage(), status), status);
     }
 
     @ExceptionHandler(value = {
@@ -62,7 +60,7 @@ public class ApiExceptionHandler {
             RuntimeException e
     ) {
         HttpStatus status = HttpStatus.BAD_REQUEST;
-        return new ResponseEntity<>(getApiExceptionObject(e.getMessage(), status), status);
+        return new ResponseEntity<>(createApiExceptionResponse(e.getMessage(), status), status);
     }
 
     @ExceptionHandler(value = {
@@ -74,12 +72,10 @@ public class ApiExceptionHandler {
         HttpStatus status = HttpStatus.BAD_REQUEST;
         var message  = e.getCause().getCause().getLocalizedMessage()
                 .split("Detail: ")[1];
-        return new ResponseEntity<>(getApiExceptionObject(message, status), status);
+        return new ResponseEntity<>(createApiExceptionResponse(message, status), status);
     }
 
-
-
-    private ApiExceptionObject getApiExceptionObject(String message, HttpStatus status) {
+    private ApiExceptionObject createApiExceptionResponse(String message, HttpStatus status) {
         return new ApiExceptionObject(
                 message,
                 status,
